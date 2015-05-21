a49842d0d97780b036aae27f5b0d03613201118a
Thomas Park <thomas@thomaspark.co>
2015-05-21 17:20:39 -0400

    set preview iframe on load and refactor js

--- scripts/snowball-admin.js
+++ scripts/snowball-admin.js
@@ -1,67 +1,50 @@
 (function($) {
 
   $(".snowball-toolbar").on("click", ".button", function() {
-    var path = snowball.path;
-    var css = path + "/styles/snowball.css";
-    var cssPreview = path + "/styles/snowball-preview.css";
-    var js = path + "/scripts/snowball.js";
     var type = $(this).data("type");
     var blockCode = snowball.blocks[type];
     var name = $(blockCode).data("name");
 
-    // alert($(this).data("type"));
-
     var block =  $("<div class='snowball-block'>" +
-                              "<div class='snowball-gui'>" +
-                                  "<div class='snowball-tinker'>" +
-                                    "<div>" +
-                                      "<div class='snowball-title'></div>" +
-                                      "<div class='snowball-delete'>&times;</div>" +
-                                    "</div>" +
-                                  "</div>" +
-                                  "<iframe class='snowball-preview'></iframe>" +
-                                "</div>" +
-                                "<div class='snowball-code'>" +
-                                  "<div class='snowball-html'></div>" +
-                                  "<div class='snowball-css'></div>" +
-                                  "<div class='snowball-js'></div>" +
-                                "</div>" +
-                              "</div>");
+                      "<div class='snowball-gui'>" +
+                          "<div class='snowball-tinker'>" +
+                            "<div>" +
+                              "<div class='snowball-title'></div>" +
+                              "<div class='snowball-delete'>&times;</div>" +
+                            "</div>" +
+                          "</div>" +
+                          "<iframe class='snowball-preview'></iframe>" +
+                        "</div>" +
+                        "<div class='snowball-code'>" +
+                          "<div class='snowball-html'></div>" +
+                          "<div class='snowball-css'></div>" +
+                          "<div class='snowball-js'></div>" +
+                        "</div>" +
+                      "</div>");
 
     block
       .addClass("snowball-block-" + type).data("type", type)
       .find(".snowball-title").text(name).end()
-      .find(".snowball-tinker").append(blockCode).end()
-      .appendTo(".snowball-main");
+      .find(".snowball-tinker").append(blockCode).end();
 
-    var stylesheet = $("<link/>").attr({"rel": "stylesheet", "href": css});
-    var stylesheetPreview = $("<link/>").attr({"rel": "stylesheet", "href": cssPreview});
-    var script = $("<script/>").attr("src", js);
-    var preview = block.find(".snowball-preview");
+    init(block);
 
-    initalize(block);
-
-    preview.contents()
-      .find("head").append(stylesheet).append(stylesheetPreview).end()
-      .find("body").append(script);
-
-    renderPreview(block);
+    block.appendTo(".snowball-main");
   });
 
-  $(".snowball-main").on("keyup", "input, textarea", debounce(function() {
-    var block = $(this).parents(".snowball-block");
-    renderPreview(block);
-  }, 250));
-
-  $(".snowball-main").on("change", "input, textarea", function() {
-    var block = $(this).parents(".snowball-block");
-    renderPreview(block);
-  });
-
-  $(".snowball-main").on("click", ".snowball-delete", function() {
-    var block = $(this).parents(".snowball-block");
-    confirmDelete(block);
-  });
+  $(".snowball-main")
+    .on("keyup", "input, textarea", debounce(function() {
+      var block = $(this).parents(".snowball-block");
+      renderPreview(block);
+    }, 250))
+    .on("change", "input, textarea", function() {
+      var block = $(this).parents(".snowball-block");
+      renderPreview(block);
+    })
+    .on("click", ".snowball-delete", function() {
+      var block = $(this).parents(".snowball-block");
+      confirmDelete(block);
+    })
 
   $(window).resize(debounce(function() {
     zoomPreview();
@@ -71,7 +54,11 @@
     zoomPreview();
   }, 250));
 
-  function initalize(block) {
+  function init(block) {
+    block.find(".snowball-preview").load(function() {
+      renderPreview(block);
+    });
+
     block.find(".wp-color-picker").wpColorPicker({
       change: debounce(function (event) {
         $(this).trigger("change");
@@ -91,12 +78,17 @@
     var fields = block.find("input[data-target], textarea[data-target]");
     var preview = block.find(".snowball-preview");
     var html = snowball.templates[type];
-    var css = "";
+
+    var path = snowball.path;
+    var css = path + "/styles/snowball.css";
+    var js = path + "/scripts/snowball.js";
+    var cssPreview = path + "/styles/snowball-preview.css";
+    var stylesheet = $("<link/>").attr({"rel": "stylesheet", "href": css});
+    var stylesheetPreview = $("<link/>").attr({"rel": "stylesheet", "href": cssPreview});
+    var script = $("<script/>").attr("src", js);
 
     fields.each(function(index, element) {
-      // var lang = $(this).data("lang");
       var target = $(this).data("target");
-
       var value = $(this).val();
 
       if ($(this).is("textarea")) {
@@ -105,14 +97,13 @@
       }
 
       html = html.replace("{{" + target + "}}", value);
-
     });
 
     preview.contents()
+      .find("head").append(stylesheet, stylesheetPreview, script).end()
       .find("body").html(html);
 
     zoomPreview(block);
-
   }
 
   function zoomPreview(block) {
