896f92feaab27ef61e5288f5fc10cfb992857c4a
Thomas Park <thomas@thomaspark.co>
2015-05-27 22:17:27 -0400

    fix image upload js to work with admin.js

--- modules/image/admin.js
+++ modules/image/admin.js
@@ -1,15 +1,23 @@
 (function($) {
-  var index;
+  $(document).ready(function() {
+    var index;
 
-  $(".upload-image-button").click(function() {
-    index = $(".upload-image-button").index($(this));
-    tb_show("", "media-upload.php?type=image&TB_iframe=true");
-    return false;
+    $(".snowball-main").on("click", ".snowball-block-image .upload-image-button", function() {
+      var block = $(this).parents(".snowball-block");
+      index = $(".snowball-block").index(block);
+      tb_show("", "media-upload.php?type=image&TB_iframe=true");
+
+      window.original_send_to_editor = window.send_to_editor;
+      window.send_to_editor = function(html){
+        var block = $(".snowball-block").eq(index);
+        imgurl = $("img", html).attr("src");
+        block.find(".upload-image").val(imgurl).trigger("change");
+        tb_remove();
+        window.send_to_editor = window.original_send_to_editor;
+      };
+
+      return false;
+    });
   });
 
-  window.send_to_editor = function(html) {
-    imgurl = $("img", html).attr("src");
-    $(".upload-image").eq(index).val(imgurl).trigger("change");
-    tb_remove();
-  };
 })(jQuery);
