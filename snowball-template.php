7f3a7925f23eee9c94471f0edaa1068a0ff3f7c4
Thomas Park <thomas@thomaspark.co>
2015-06-04 11:34:04 -0400

    remove whitespace in article

--- snowball-template.php
+++ snowball-template.php
@@ -8,11 +8,11 @@ setup_postdata($post);
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js">
 <head>
-  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width">
   <title><?php wp_title('|', true, 'right'); ?></title>
   <link rel="profile" href="http://gmpg.org/xfn/11">
-  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+  <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
   <?php do_action("snowball_enqueue_stylesheets"); ?>
   <script src="<?php echo includes_url('js/jquery/jquery.js'); ?>"></script>
   <?php do_action("snowball_enqueue_scripts"); ?>
