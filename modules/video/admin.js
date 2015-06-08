46e77d83d829d172557bc6af902ff8f7ab1dc6a6
Sukrit Chhabra <sukrit.chhabra@gmail.com>
2015-06-08 16:54:59 -0400

    Added Scoped Style and Video Options to Video Block
    * Added Scoped style to template.html
    * Added options for controls to admin.html

--- modules/video/admin.js
+++ modules/video/admin.js
@@ -5,7 +5,7 @@
     var videoID = parseVideoURL(videoUrl);
 
     $(this).find(".video-id").val(videoID);
-    $(this).find(".video-id").trigger("change");
+    $(this).trigger("render");
   });
 
   $(".snowball-main").on("change keyup", ".snowball-block-video .video-url", function() {
@@ -14,20 +14,7 @@
     var videoID = parseVideoURL(videoUrl);
 
     block.find(".video-id").val(videoID);
-    block.find(".video-id").trigger("change");
-  });
-
-  $(".snowball-main").on("click", ".snowball-block-video .full-width-checkbox", function() {
-    var block = $(this).parents(".snowball-block-video");
-    var checkedStatus = $(this).prop("checked");
-
-    if (checkedStatus == false) {
-      block.find(".custom-style").val("");
-    } else {
-      block.find(".custom-style").val("width:100%; height:55.6vw;");
-    }
-
-    block.find(".video-url").trigger("keyup");
+    block.trigger("render");
   });
 
   function parseVideoURL(videoUrl) {
