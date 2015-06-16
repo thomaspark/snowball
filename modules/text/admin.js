751bd568bea7b126e0427c6bb5f1c0d7424ebb70
Thomas Park <thomas@thomaspark.co>
2015-06-16 18:28:51 -0400

    add editor toolbar to text block, closes #2

---
+++ modules/text/admin.js
@@ -0,0 +1,23 @@
+(function($) {
+    var counter = 0;
+    QTags.addButton("eg_hr", "hr", "<hr />", "", "h", "Horizontal rule line", 70);
+
+    $(".snowball-main").on("open", ".snowball-block-text", function() {
+      var textarea = $(this).find("textarea");
+      var id = "snowball-block-text-textarea-" + counter;
+      textarea.attr("id", id);
+      counter++;
+
+      quicktags({
+        id : id,
+        buttons: "strong,em,link,blockquote,del,code,ul,ol,li,code,close"
+      });
+
+      QTags._buttonsInit();
+    });
+
+    $(".snowball-main").on("click", ".snowball-block-text .quicktags-toolbar .button", function() {
+      $(this).parents(".snowball-block").trigger("render");
+    });
+
+})(jQuery);
