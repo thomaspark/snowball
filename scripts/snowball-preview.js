ef86f0267ea13150c09e1b89e1631ee26b73f2d8
Thomas Park <thomas@thomaspark.co>
2015-06-29 23:02:48 -0400

    reset script context to preview iframe

--- scripts/snowball-preview.js
+++ scripts/snowball-preview.js
@@ -1,7 +1,24 @@
-if (typeof(jQuery) == "undefined"){
-  var body = document.getElementsByTagName("body")[0];
+var body = document.getElementsByTagName("body")[0];
+
+// set jquery variable within the iframe's context
+if (typeof(jQuery) == "undefined") {
   var jQuery = function (selector) { return parent.jQuery(selector, body); };
   var $ = jQuery;
 }
 
-$("a").attr("target", "_blank");
+// reset script tags within the iframe's context
+$("script").each(function() {
+  var script = document.createElement("script");
+  var attr = $(this).attr("src");
+
+  if (attr) {
+    var src = $(this).attr("src");
+    script.setAttribute("src", src);
+  } else {
+    script.innerHTML = $(this).html();
+  }
+
+  body.appendChild(script);
+}).remove();
+
+$("a[href]").attr("target", "_blank");
