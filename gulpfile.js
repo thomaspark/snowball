ab7130020c77c6c8bad41c35e53b5d3d3ceddfa7
Brian Lee <bl389@drexel.edu>
2015-07-02 10:13:53 -0400

    # This is a combination of 2 commits. # The first commit's message is:
    Setup gulp with minified files being used
    
    # This is the 2nd commit message:
    
    minifed fixedsticky files

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,25 @@ var uglify = require('gulp-uglify');
 var browserSync = require('browser-sync');
 var rename = require('gulp-rename');
 var minifyCss = require('gulp-minify-css');
+//var glob = require('glob');
+//var StreamQueue = require('streamqueue');
+var plumber = require('gulp-plumber');
+var gutil = require('gulp-util');
+var notify = require('gulp-notify');
 
 var WORDPRESSURL = "http://localhost:8888/wordpresstest/wp-admin/edit.php?post_type=snowball";
+
+var onError = function (err) {  
+  gutil.beep();
+  console.log(err);
+};
 
 gulp.task('minify', function () {
     //var negateMinCSS = 'styles/!(*.min.css)'; for retrieving all non .min.css
     gulp.src('styles/*.css')
+        .pipe(plumber({
+          errorHandler: onError
+        }))
         .pipe(minifyCss())
         .pipe(rename(function(path) {
             if (path.extname == ".css") {
@@ -21,15 +34,71 @@ gulp.task('minify', function () {
 gulp.task('uglify', function() {
 	//var negateMinJS = 'scripts/!(*.min.js)'; for retrieving all non .min.js
     gulp.src('./scripts/*.js')
+        .pipe(plumber({
+          errorHandler: notify.onError({
+            title: 'Error!',
+            message: '<%= error.message %>',
+            sound: 'Beep'
+          })
+        }))
 		.pipe(uglify())
-		.pipe(rename(function (path) {
-			if (path.extname == ".js") {
+        .pipe(rename(function (path) {
+            if (path.extname == ".js") {
                 path.basename += '.min';
-			}
-		}))
+            }
+        }))
 		.pipe(gulp.dest("./scripts/min/"));
 });
 
+gulp.task('minify-codemirror', function() {
+  var dir = "./lib/codemirror/**/";
+  // minify js
+  gulp.src([dir + '*.js', '!' + dir + '*.min.js'])
+        .pipe(plumber({
+            errorHandler: onError
+        }))
+        .pipe(uglify())
+        .pipe(rename(function (path) {
+            path.basename += '.min';
+        }))
+        .pipe(gulp.dest("./lib/codemirror/"));
+ 
+  // minify css
+  gulp.src([dir + '*.css', '!' + dir + '*.min.css'])
+        .pipe(plumber({
+            errorHandler: onError
+        }))
+        .pipe(minifyCss())
+        .pipe(rename(function (path) {
+            path.basename += '.min';
+        }))
+        .pipe(gulp.dest("./lib/codemirror/"));
+});
+
+gulp.task('minify-fixed-sticky', function() {
+  var dir = "./lib/fixed-sticky/*";
+  // minify js
+  gulp.src([dir + '*.js', '!' + dir + '*.min.js'])
+        .pipe(plumber({
+            errorHandler: onError
+        }))
+        .pipe(uglify())
+        .pipe(rename(function (path) {
+            path.basename += '.min';
+        }))
+        .pipe(gulp.dest("./lib/fixed-sticky/"));
+ 
+  // minify css
+  gulp.src([dir + '*.css', '!' + dir + '*.min.css'])
+        .pipe(plumber({
+            errorHandler: onError
+        }))
+        .pipe(minifyCss())
+        .pipe(rename(function (path) {
+            path.basename += '.min';
+        }))
+        .pipe(gulp.dest("./lib/fixed-sticky/"));
+});
 /*
     This will open a new tab of your wordpress site whenever
     the browersync server is restarted.
@@ -47,3 +116,52 @@ gulp.task('default', ['uglify', 'minify', 'browser-sync'], function () {
     gulp.watch("inc/*.php").on('change', browserSync.reload);
     gulp.watch("modules/*/*").on('change', browserSync.reload);
 });
+
+  /*
+    TODO:
+    Can't figure out why the code keeps outputting:
+    [TypeError: Invalid non-string/buffer chunk]
+    
+    Possible solution would be trying a different streamqueue.
+    This is the codemirror solution for solving globs
+  */
+  //gulp.task('minify-codemirror-glob', function() {
+  // var libDir = glob.sync('lib/codemirror/**/');
+  // var queue = new StreamQueue();
+  // libDir.forEach(function(folder) {
+  //   var dirName = folder.match(/.+\/(.+)\/$/)[1];
+  //   queue.queue(
+  //     gulp.src([folder + '*.js', '!' + folder + '*.min.js'])
+  //       .pipe(plumber({
+  //           errorHandler: onError
+  //       }))
+  //       .pipe(uglify())
+  //       .pipe(rename(function (path) {
+  //           if (path.extname == ".js") {
+  //               path.basename += '.min';
+  //           }
+  //       }))
+  //       .pipe(gulp.dest(folder))
+  // );
+
+    // queue.queue(
+    //   gulp.src([folder + '*.css', '!' + folder + '*.min.css'])
+    //       .pipe(plumber({
+    //         errorHandler: notify.onError({
+    //           title: 'Error!',
+    //           message: '<%= error.message %>',
+    //           sound: 'Beep'
+    //         })
+    //       }))
+    //       .pipe(minifyCss())
+    //       .pipe(rename(function (path) {
+    //         path.basename += ".min";
+    //       }))
+    //       .pipe(gulp.dest(folder))
+    // );
+
+  // });
+
+  // return queue;
+//});
+
--- inc/snowball-enqueue.php
+++ inc/snowball-enqueue.php
@@ -3,7 +3,7 @@
 function snowball_add_stylesheets() {
   echo '<link rel="stylesheet" href="' . plugins_url('snowball/lib/fluidbox/css/fluidbox.css') . '">';
   echo '<link rel="stylesheet" href="' . plugins_url('snowball/lib/font-awesome/css/font-awesome.min.css') . '">';
-  echo '<link rel="stylesheet" href="' . plugins_url('snowball/styles/snowball.css') . '">';
+  echo '<link rel="stylesheet" href="' . plugins_url('snowball/styles/min/snowball.min.css') . '">';
 }
 add_action('snowball_enqueue_stylesheets', 'snowball_add_stylesheets');
 
@@ -11,7 +11,7 @@ function snowball_add_scripts() {
   echo '<script src="' . includes_url('js/jquery/jquery.js') . '"></script>';
   echo '<script src="' . plugins_url('snowball/lib/fluidbox/jquery.fluidbox.min.js') . '"></script>';
   echo '<script src="' . plugins_url('snowball/lib/scoper/scoper.js') . '"></script>';
-  echo '<script src="' . plugins_url('snowball/scripts/snowball.js') . '"></script>';
+  echo '<script src="' . plugins_url('snowball/scripts/min/snowball.min.js') . '"></script>';
 }
 add_action('snowball_enqueue_scripts', 'snowball_add_scripts');
 
@@ -20,21 +20,21 @@ function snowball_admin_add_scripts_and_stylesheets($hook) {
     wp_enqueue_style('wp-color-picker');
     wp_enqueue_style('thickbox');
     wp_enqueue_style('fontawesome-css', plugins_url('snowball/lib/font-awesome/css/font-awesome.min.css'));
-    wp_enqueue_style('codemirror-css', plugins_url('snowball/lib/codemirror/lib/codemirror.css'));
-    wp_enqueue_style('codemirror-theme-monokai', plugins_url('snowball/lib/codemirror/theme/monokai.css'));
-    wp_enqueue_style('fixedsticky-css', plugins_url('snowball/lib/fixed-sticky/fixedsticky.css'));
+    wp_enqueue_style('codemirror-css', plugins_url('snowball/lib/codemirror/lib/codemirror.min.css'));
+    wp_enqueue_style('codemirror-theme-monokai', plugins_url('snowball/lib/codemirror/theme/monokai.min.css'));
+    wp_enqueue_style('fixedsticky-css', plugins_url('snowball/lib/fixed-sticky/fixedsticky.min.css'));
     wp_enqueue_style('handsontable-css', plugins_url('snowball/lib/handsontable/handsontable.full.min.css'));
-    wp_enqueue_style('snowball-css', plugins_url('snowball/styles/snowball-admin.css'));
+    wp_enqueue_style('snowball-css', plugins_url('snowball/styles/min/snowball-admin.min.css'));
 
     wp_enqueue_script('media-upload');
     wp_enqueue_script('thickbox');
-    wp_enqueue_script('codemirror-js', plugins_url('snowball/lib/codemirror/lib/codemirror.js'));
-    wp_enqueue_script('codemirror-mode-javascript', plugins_url('snowball/lib/codemirror/mode/javascript.js'));
-    wp_enqueue_script('codemirror-mode-css', plugins_url('snowball/lib/codemirror/mode/css.js'));
-    wp_enqueue_script('codemirror-mode-xml', plugins_url('snowball/lib/codemirror/mode/xml.js'));
-    wp_enqueue_script('fixedsticky-js', plugins_url('snowball/lib/fixed-sticky/fixedsticky.js'), array('jquery'), '', true);
+    wp_enqueue_script('codemirror-js', plugins_url('snowball/lib/codemirror/lib/codemirror.min.js'));
+    //wp_enqueue_script('codemirror-mode-javascript', plugins_url('snowball/lib/codemirror/mode/javascript.min.js'));
+    wp_enqueue_script('codemirror-mode-css', plugins_url('snowball/lib/codemirror/mode/css.min.js'));
+    wp_enqueue_script('codemirror-mode-xml', plugins_url('snowball/lib/codemirror/mode/xml.min.js'));
+    wp_enqueue_script('fixedsticky-js', plugins_url('snowball/lib/fixed-sticky/fixedsticky.min.js'), array('jquery'), '', true);
     wp_enqueue_script('handsontable-js', plugins_url('snowball/lib/handsontable/handsontable.full.min.js'), array(), '', true);
-    wp_enqueue_script('snowball-js', plugins_url('snowball/scripts/snowball-admin.js'), array('jquery', 'jquery-ui-sortable', 'wp-color-picker'), '', true);
+    wp_enqueue_script('snowball-js', plugins_url('snowball/scripts/min/snowball-admin.min.js'), array('jquery', 'jquery-ui-sortable', 'wp-color-picker'), '', true);
   }
 }
 add_action('admin_enqueue_scripts', 'snowball_admin_add_scripts_and_stylesheets');
@@ -44,7 +44,7 @@ function snowball_admin_ajax_script($hook) {
     global $post;
     global $path;
 
-    wp_enqueue_script('ajax-script', plugins_url('/scripts/snowball-ajax.js', dirname(__FILE__)), array('jquery'));
+    wp_enqueue_script('ajax-script', plugins_url('/scripts/min/snowball-ajax.min.js', dirname(__FILE__)), array('jquery'));
 
     // in JavaScript, object properties are accessed as ajax_object.ajax_url, ajax_object.we_value
     wp_localize_script('ajax-script', 'ajax_object',
