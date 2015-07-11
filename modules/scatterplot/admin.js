4c245f07434a93202d4d7b492167611fc89bd07e
Thomas Park <thomas@thomaspark.co>
2015-07-11 01:24:14 -0400

    add scatterplot module

---
+++ modules/scatterplot/admin.js
@@ -0,0 +1,120 @@
+(function($) {
+
+  $("#snowball-main").on("open", ".snowball-block-scatterplot", function() {
+    var block = $(this);
+    var size = $(this).find(".size").val() + "px";
+    $(this).find(".size-output").text(size);
+
+    loadData(block);
+  });
+
+  $("#snowball-main").on("input change", ".snowball-block-scatterplot .size", function() {
+    var block = $(this).closest(".snowball-block-scatterplot");
+    var size = $(this).val() + "px";
+
+    block.find(".size-output").text(size);
+    block.trigger("render");
+  });
+
+  $("#snowball-main").on("rendered", ".snowball-block-scatterplot", function() {
+    var block = $(this);
+    var iframe = block.find(".snowball-preview");
+    var map = iframe.contents().find(".chart");
+    var draw = iframe[0].contentWindow.drawScatterplot;
+
+    if (map && draw) {
+      draw(iframe.contents().find(".snowball-block-scatterplot"));
+    }
+  });
+
+  function loadData(block) {
+    var container = block.find(".table").get(0);
+    var json = block.find("[data-target='json']").val();
+
+    if (json) {
+      var data = JSON.parse(json);
+      initTable(block, container, data); 
+    } else {
+      var url = snowball.pluginsUrl + "/modules/scatterplot/snowline.json";
+
+      $.getJSON(url, function(data) {
+        initTable(block, container, data);
+      });
+    }
+  }
+
+  function reloadData(block) {
+    var container = block.find(".table").get(0);
+    var hot = block.data("hot");
+    var json = block.find("[data-target='json']").val();
+    var colHeaders = ["Label", "X", "Y"];
+
+    if (json) {
+      var data = JSON.parse(json);
+      hot.updateSettings({
+        colHeaders: colHeaders
+      });
+      hot.loadData(data);
+      hot.render();
+    } else {
+      var url = snowball.pluginsUrl + "/modules/choropleth/snowline.json";
+
+      $.getJSON(url, function(data) {
+        hot.updateSettings({
+          colHeaders: colHeaders,
+        });
+        hot.loadData(data);
+        hot.render();
+      });
+    }
+  }
+
+  function initTable(block, container, data) {
+    var colHeaders = ["Label", "X", "Y"];
+    var columns = [{data: "Label", type: "text"}, {data: "X", type: "numeric"}, {data: "Y", type: "numeric"}];
+
+    var hot = new Handsontable(container, {
+      data: data,
+      rowHeaders: false,
+      fixedRowsTop: 0,
+      colHeaders: colHeaders,
+      columns: columns,
+      columnSorting: false,
+      manualColumnMove: false,
+      manualColumnResize: false,
+      contextMenu: true,
+      multiSelect: true,
+      persistantState: true,
+      fillHandle: true,
+      observeChanges: true,
+      search: true,
+      undo: true,
+      readOnly: false,
+      stretchH: "all",
+      afterChange: function(changes, source) {
+        var data = this.getData();
+        refreshOnChange(block, data);
+        if (source !== "loadData") {
+          block.trigger("render");
+        }
+      },
+      afterLoadData: function() {
+        var data = this.getData();
+        refreshOnChange(block, data);
+      },
+      afterInit: function() {
+        block.trigger("render");
+      }
+    });
+
+    block.data("hot", hot);
+  }
+
+  function refreshOnChange(block, data, source) {
+    var generatedJSON = data;
+    var jsonString = JSON.stringify(generatedJSON);
+
+    block.find("[data-target='json']").val(jsonString);
+  }
+
+})(jQuery);
---
+++ modules/scatterplot/template.js
@@ -0,0 +1,81 @@
+(function($) {
+  $(document).ready(function() {
+
+    var choropleths = $(".snowball-block-scatterplot");
+    choropleths.each(function() {
+      drawScatterplot($(this));
+    });
+
+  });
+})(jQuery);
+
+function drawScatterplot(block) {
+  var json = block.find(".json").val();
+
+  if (json) {
+    var container = block.find(".chart").get(0);
+    var size = block.find(".size").val();
+    var data = JSON.parse(json);
+    var dataset = [];
+    var xdata = [];
+    var ydata = [];
+
+    data.forEach(function(d) {
+      dataset.push([d.X, d.Y]);
+      xdata.push(d.X);
+      ydata.push(d.Y);
+    });
+
+    var margin = {top: 40, right: 40, bottom: 60, left: 60};
+    var width = 800 - margin.left - margin.right;
+    var height = 400 - margin.top - margin.bottom;
+
+    var x = d3.scale.linear()
+              .domain([0, d3.max(xdata)])
+              .range([ 0, width ]);
+
+    var y = d3.scale.linear()
+              .domain([0, d3.max(ydata)])
+              .range([ height, 0 ]);
+
+    var chart = d3.select(container)
+      .append('svg:svg')
+      .attr('width', width + margin.right + margin.left)
+      .attr('height', height + margin.top + margin.bottom)
+      .attr('class', 'chart');
+
+    var main = chart.append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+      .attr('width', width)
+      .attr('height', height)
+      .attr('class', 'main');
+
+    var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient('bottom');
+
+    main.append('g')
+      .attr('transform', 'translate(0,' + height + ')')
+      .attr('class', 'main axis date')
+      .call(xAxis);
+
+    var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient('left');
+
+    main.append('g')
+      .attr('transform', 'translate(0,0)')
+      .attr('class', 'main axis date')
+      .call(yAxis);
+
+    var g = main.append("svg:g"); 
+
+    g.selectAll("scatter-dots")
+      .data(ydata)
+      .enter().append("svg:circle")
+          .attr("cy", function (d) { return y(d); } )
+          .attr("cx", function (d,i) { return x(xdata[i]); } )
+          .attr("r", size);
+
+  }
+}
