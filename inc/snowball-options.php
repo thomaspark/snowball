2f758226b58b208087e7b4a1ac8840b2bbeb130c
Brian Lee <bl389@drexel.edu>
2015-07-31 10:52:45 -0400

    Created initial options page

---
+++ inc/snowball-options.php
@@ -0,0 +1,20 @@
+<div class="wrap">
+	<h2>Snowball Options</h2>
+	<div>What you want</div>
+	<div>What kind of settings shall there be</div>
+	<form name="hello" method="post" action="">
+		<label for="hello">Sample Option things</label>
+		<input type="text" name="hello" value="Hey there">
+		<br>
+		<label for="hello_word">Hello Word</label>
+		<input type="text" name="hello_word" value="">
+		<br>
+		<label for="word_hello">Word Hello</label>
+		<input type="text" name="word_hello" value="">
+		<p>
+			<input class="button-primary" type="submit" name="hello_submit" value="Save" /> 
+		</p>
+
+		<div class="message"></div>
+	</form>
+</div>
---
+++ scripts/snowball-options.js
@@ -0,0 +1,5 @@
+(function($) {
+	$(document).ready(function() {
+		$("h2").text("This javascript file and the snowball.css files was added to this settings page.");
+	});
+})(jQuery);
