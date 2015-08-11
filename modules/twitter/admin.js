24c9b0441330e9c289137207bc4d55f45fde1bc5
Thomas Park <thomas@thomaspark.co>
2015-08-11 14:22:15 -0400

    add twitter module

---
+++ modules/twitter/admin.js
@@ -0,0 +1,40 @@
+(function($) {
+
+  $("#snowball-main").on("open", ".snowball-block-twitter", function() {
+    var tweetUrl = $(this).find("[data-target='tweet-url']").val().trim();
+    var tweetID = parseTweetUrl(tweetUrl);
+
+    $(this).find("[data-target='id']").val(tweetID);
+    $(this).trigger("render");
+  });
+
+  $("#snowball-main").on("change keyup", ".snowball-block-twitter [data-target='tweet-url']", function() {
+    var block = $(this).closest(".snowball-block-twitter");
+    var tweetUrl = $(this).val().trim();
+    var tweetID = parseTweetUrl(tweetUrl);
+
+    block.find("[data-target='id']").val(tweetID);
+  });
+
+  $("#snowball-main").on("rendered", ".snowball-block-twitter", function() {
+    var block = $(this);
+    var iframe = block.find(".snowball-preview");
+    var createTweet = iframe[0].contentWindow.createTweet;
+
+    if (createTweet) {
+      createTweet(iframe.contents().find(".snowball-block-twitter"));
+    }
+  });
+
+  function parseTweetUrl(url) {
+    var re = /^https?:\/\/twitter.com\/(\w+)\/status(es)?\/(\d+)$/;
+    var matches = re.exec(url);
+
+    if (matches && matches[3]) {
+      return matches[3];
+    } else {
+      return "";
+    }
+  }
+
+})(jQuery);
---
+++ modules/twitter/template.js
@@ -0,0 +1,31 @@
+(function($) {
+  var url = "https://platform.twitter.com/widgets.js";
+
+  $.getScript(url, function() {
+    $(".snowball-block-twitter").each(function() {
+      createTweet($(this));
+    }); 
+  });
+
+})(jQuery);
+
+function createTweet(block) {
+  var tweet = block.find(".tweet").empty();
+  var id = tweet.attr("data-id");
+
+  if (id) {
+    var theme = tweet.attr("data-theme");
+    var linkColor = tweet.attr("data-linkColor");
+    var cards = tweet.attr("data-cards") ? tweet.attr("data-cards") : "show";
+    var conversation = tweet.attr("data-conversation") ? tweet.attr("data-conversation") : "show";
+ 
+    twttr.widgets.createTweet(id, tweet[0], 
+      {
+        theme: theme,
+        linkColor: linkColor,
+        cards: cards,
+        conversation: conversation,
+        align: "center"
+      });
+  }
+}
