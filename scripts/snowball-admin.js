1a9dd3548201a419df180064f9194049ad3163da
Noel Forte <ntforte@gmail.com>
2015-10-07 21:11:41 -0400

    Reccommendation
    You'll have to minify again, but this is what I'd reccommend.

--- scripts/snowball-admin.js
+++ scripts/snowball-admin.js
@@ -2,7 +2,7 @@
   var changesMade = false;
   var actions = [];
 
-  jQuery(document).ready(function() {
+  $(document).ready(function() {
     if ((snowball.savedblocks.length !== 0) && (snowball.savedblocks !== "null")) {
       $("#snowball_meta").removeClass("closed");
 
@@ -11,7 +11,9 @@
     } else {
       snowball.savedblocks = [];
     }
+  });
 
+  $(window).load(function() {
     setHandlers();
   });
 
