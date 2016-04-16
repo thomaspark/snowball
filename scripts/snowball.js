88aed8e26cba1d16d6b7c187e688c661de5aa0ac
Thomas Park <thomas@thomaspark.co>
2016-04-15 20:37:29 -0400

    fix selector syntax for smooth scrolling anchor links

--- scripts/snowball.js
+++ scripts/snowball.js
@@ -10,7 +10,7 @@
 
     lightboxes.fluidbox();
 
-    $("a[href*=#]:not([href=#])").click(function() {
+    $("a[href*=\\#]:not([href=\\#])").click(function() {
       if (location.pathname.replace(/^\//,"") == this.pathname.replace(/^\//,"") && location.hostname == this.hostname) {
         var target = $(this.hash);
         target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
